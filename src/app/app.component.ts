2390032a2d3aaa4ea002a0caec059da691dee8c3
Himar Barquín <hbarquin@academiahollerith.es>
2024-03-09 12:34:28 +0000

    build: created main page structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContainerComponent } from './container/container.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    ContainerComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
