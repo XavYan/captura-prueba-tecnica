f80f6fdca99b6c08fc7145f72ca1703bcef94cc6
Himar Barquín <hbarquin@academiahollerith.es>
2024-03-10 11:56:55 +0000

    build: changed coordinates to match a closer view

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,12 +11,12 @@ import L from 'leaflet'
 export class HomeComponent implements OnInit {
   ngOnInit(): void {
     console.log("Opened HomeComponent");
-    var map = L.map('map').setView([51.505, -0.09], 13);
+    var map = L.map('map').setView([28.467623, -16.2589725], 13);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
-    var marker = L.marker([51.5, -0.09]).addTo(map);
+    var marker = L.marker([28.467623, -16.2589725]).addTo(map);
     marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
   }
 }
