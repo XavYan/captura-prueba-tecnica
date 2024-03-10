15ad6a6a5ba8c975e5bddfa1cf902c6be093b30c
Himar Barquín <hbarquin@academiahollerith.es>
2024-03-10 20:28:16 +0000

    feat: created filter input to markers table

--- src/app/marker-table/marker-table.component.ts
+++ src/app/marker-table/marker-table.component.ts
@@ -1,10 +1,15 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, NgModel } from '@angular/forms';
 import { Marker } from '../interfaces/marker';
 
 @Component({
   selector: 'app-marker-table',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './marker-table.component.html',
   styleUrl: './marker-table.component.css'
 })
@@ -12,10 +17,21 @@ export class MarkerTableComponent {
   @Input() markerList: Marker[] = [];
   @Output() locateMarker = new EventEmitter<Marker>();
 
+  projectSearch: string = "";
+
   onMarkerRowClick(e: MouseEvent) {
     const index = (e.currentTarget as HTMLElement).getAttribute('data-row');
     if (index !== null) {
       this.locateMarker.emit(this.markerList.at(Number(index)));
     }
+  }
+
+  filterTable(): Marker[] {
+    if (this.projectSearch.length === 0) {
+      return this.markerList;
+    }
+    return this.markerList.filter(
+      marker => marker.project.toLowerCase().includes(this.projectSearch.toLowerCase())
+    );
   }
 }
