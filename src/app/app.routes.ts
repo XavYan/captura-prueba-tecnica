6f89fe6fea3f28985642d4225c44bb053b2ffc9b
Himar Barquín <hbarquin@academiahollerith.es>
2024-03-10 19:18:06 +0000

    fix: added new page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { HowToUseComponent } from './how-to-use-component/how-to-use.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent,
         title: 'CAPTURA - Página princial'
+    },
+    {
+        path: 'how-to-use',
+        component: HowToUseComponent,
+        title: 'CAPTURA - Cómo funciona'
     }
 ];
